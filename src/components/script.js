db0d9a53a491219c83ad59acb649b73be77878f2
Elton <nocpolaco@gmail.com>
2022-09-16 23:17:42 -0300

    update

---
+++ src/components/script.js
@@ -0,0 +1,6 @@
+$('.nordeste').Click(function() {
+    $('.menuLateral ul .itensNordeste').toggleClass('mostra');
+});
+$('.sudeste').Click(function() {
+    $('.menuLateral ul .itensSudeste').toggleClass('mostra');
+});
